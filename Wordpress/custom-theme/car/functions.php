18c933f8720e0dc493a83f702f1019054010bb4b
Nikolai2025 <124783539+Nikolai2025@users.noreply.github.com>
2026-04-10 15:15:06 +0200

    Add files via upload

---
+++ Wordpress/custom-theme/car/functions.php
@@ -0,0 +1,58 @@
+<?php
+ 
+function wptheme_script_enqueue() {
+ 
+    wp_enqueue_style(
+        'customstyle',
+        get_template_directory_uri() . '/css/app.css',
+        array(),
+        '1.0',
+        'all'
+    );
+ 
+    wp_enqueue_script(
+        'customjs',
+        get_template_directory_uri() . '/js/app.js',
+        array(),
+        '1.0',
+        true
+    );
+ 
+}
+ add_action('wp_enqueue_scripts', 'wptheme_script_enqueue');
+
+ 
+
+function my_register_cakes_post_type() {
+
+    register_post_type('car', [
+
+        'labels' => [
+
+            'name' => 'Cars',
+
+            'singular_name' => 'Car',
+
+        ],
+
+        'public' => true,
+
+        'has_archive' => true,
+
+        'show_in_rest' => true, // important for Gutenberg + REST API
+
+        'menu_icon' => 'dashicons-car',
+
+        'supports' => ['title', 'editor', 'thumbnail', 'excerpt'],
+
+        'rewrite' => ['slug' => 'cars'],
+
+    ]);
+
+}
+
+add_action('init', 'my_register_cakes_post_type');
+
+
+
+?>
