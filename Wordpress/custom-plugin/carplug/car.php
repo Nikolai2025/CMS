439bb829954d3b8dd3c3b84847524994ccee191c
Nikolai2025 <124783539+Nikolai2025@users.noreply.github.com>
2026-04-10 15:17:47 +0200

    Add files via upload

---
+++ Wordpress/custom-plugin/carplug/car.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Hello car
+Plugin URI: http://wordpress.org/plugins/hello-car/
+Description: i love cars. When activated you will randomly see a lyric from <cite>Hello, car</cite> in the upper right of your admin screen on every page.
+Author: Nikolai langergaard
+Version: 1.7.2
+Author URI: http://ma.tt/
+Text Domain: hello-car
+*/
